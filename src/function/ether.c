chris2511/libusbgx

commit 36e71e1e1ea9fb18d4765d0376329dcde90c8c91

Rename the net class variables to class_
Using C++ keywords like "class" as variable names causes
build failures when libusbgx is used in C++ projects.

Reported-by: Thomas Moeller (@moether)
Reported-in: https://github.com/linux-usb-gadgets/libusbgx/issues/65#issuecomment-1296790932
Fixes: commit 4ca4a7699b1226e7e00c4e0a346b11de875a0ff0
Fixes: commit efdd7a657c32f08830548bdff5784ccbb65757ec

src/function/ether.c
@@ -67,7 +67,7 @@ static struct {
 	[USBG_F_NET_HOST_ADDR] = NET_ETHER_ADDR_ATTR(host_addr),
 	[USBG_F_NET_IFNAME] = NET_RO_STRING_ATTR(ifname),
 	[USBG_F_NET_QMULT] = NET_DEC_ATTR(qmult),
-	[USBG_F_NET_CLASS] = NET_DEC_ATTR(class),
+	[USBG_F_NET_CLASS] = NET_DEC_ATTR(class_),
 	[USBG_F_NET_SUBCLASS] = NET_DEC_ATTR(subclass),
 	[USBG_F_NET_PROTOCOL] = NET_DEC_ATTR(protocol)
 };

include/usbg/function/net.h
@@ -29,7 +29,7 @@ struct usbg_f_net_attrs {
 	struct ether_addr host_addr;
 	const char *ifname;
 	int qmult;
-	unsigned int class;
+	unsigned int class_;
 	unsigned int subclass;
 	unsigned int protocol;
 };
@@ -51,7 +51,7 @@ union usbg_f_net_attr_val {
 	struct ether_addr host_addr;
 	const char *ifname;
 	int qmult;
-	unsigned int class;
+	unsigned int class_;
 	unsigned int subclass;
 	unsigned int protocol;
 };
@@ -233,25 +233,25 @@ static inline int usbg_f_net_set_qmult(usbg_f_net *nf, int qmult)
 /**
  * @brief Get the value of usb function class
  * @param[in] nf Pointer to net function
- * @param[out] class Current class identification
+ * @param[out] class_ Current class identification
  * @return 0 on success usbg_error if error occurred.
  */
-static inline int usbg_f_net_get_class(usbg_f_net *nf, unsigned int *class)
+static inline int usbg_f_net_get_class(usbg_f_net *nf, unsigned int *class_)
 {
 	return usbg_f_net_get_attr_val(nf, USBG_F_NET_CLASS,
-				       (union usbg_f_net_attr_val *)class);
+				       (union usbg_f_net_attr_val *)class_);
 }
 
 /**
  * @brief Set the value of usb function class
  * @param[in] nf Pointer to net function
- * @param[in] class Class identification
+ * @param[in] class_ Class identification
  * @return 0 on success usbg_error if error occurred.
  */
-static inline int usbg_f_net_set_class(usbg_f_net *nf, unsigned int class)
+static inline int usbg_f_net_set_class(usbg_f_net *nf, unsigned int class_)
 {
 	return usbg_f_net_set_attr_val(nf, USBG_F_NET_CLASS,
-				       USBG_F_NET_INT_TO_ATTR_VAL(class));
+				       USBG_F_NET_INT_TO_ATTR_VAL(class_));
 }
 
 /**

tests/usbg-test.c
@@ -1069,7 +1069,7 @@ static void push_net_attrs(struct test_function *func,
 	PUSH_FILE_STR(path, content);
 
 	safe_asprintf(&path, "%s/%s/class", func->path, func->name);
-	safe_asprintf(&content, "%d\n", attrs->class);
+	safe_asprintf(&content, "%d\n", attrs->class_);
 	PUSH_FILE_STR(path, content);
 
 	safe_asprintf(&path, "%s/%s/subclass", func->path, func->name);
@@ -1142,7 +1142,7 @@ static void pull_function_net_attrs(struct test_function *func,
 	EXPECT_WRITE_STR(path, content);
 
 	safe_asprintf(&path, "%s/%s/class", func->path, func->name);
-	safe_asprintf(&content, "%d\n", attrs->class);
+	safe_asprintf(&content, "%d\n", attrs->class_);
 	EXPECT_WRITE_STR(path, content);
 
 	safe_asprintf(&path, "%s/%s/subclass", func->path, func->name);
@@ -1341,7 +1341,7 @@ void assert_f_net_attrs_equal(struct usbg_f_net_attrs *actual,
 	assert_ether_addrs_equal(&actual->host_addr, &expected->host_addr);
 	assert_string_equal(actual->ifname, expected->ifname);
 	assert_int_equal(actual->qmult, expected->qmult);
-	assert_int_equal(actual->class, expected->class);
+	assert_int_equal(actual->class_, expected->class_);
 	assert_int_equal(actual->subclass, expected->subclass);
 	assert_int_equal(actual->protocol, expected->protocol);
 }

commit 59952eab8a36b59c9e330952f8441c99cd13b824

Fix property name "class" of ethernet gadget
commit 36e71e1e1ea9fb18d4765d0376329dcde90c8c91
renamed all "class" to "class_" for C++ compatibility.
However, the file name in the configfs should remain "class":

  openat(AT_FDCWD, "/sys/kernel/config/usb_gadget/g1/functions/rndis.usb0/class_", O_RDONLY|O_LARGEFILE) = -1 ENOENT (No such file or directory)

Reproducible by running the examples:
  gadget-rndis-os-desc; show-gadgets

Fixes: commit 36e71e1e1ea9fb18d4765d0376329dcde90c8c91

src/function/ether.c
@@ -23,16 +23,17 @@ struct usbg_f_net {
 	struct usbg_function func;
 };
 
-#define NET_DEC_ATTR(_name)						\
+#define NET_DEC_ATTR_(_name,_elname)					\
 	{								\
 		.name = #_name,						\
 		.ro = false,					        \
-		.offset = offsetof(struct usbg_f_net_attrs, _name),     \
+		.offset = offsetof(struct usbg_f_net_attrs, _elname),   \
 		.get = usbg_get_dec,				        \
 		.set = usbg_set_dec,				        \
 		.import = usbg_get_config_node_int,	                \
 		.export = usbg_set_config_node_int,		        \
 	}
+#define NET_DEC_ATTR(_name) NET_DEC_ATTR_(_name,_name)			\
 
 #define NET_RO_STRING_ATTR(_name)					\
 	{								\
@@ -67,7 +68,7 @@ static struct {
 	[USBG_F_NET_HOST_ADDR] = NET_ETHER_ADDR_ATTR(host_addr),
 	[USBG_F_NET_IFNAME] = NET_RO_STRING_ATTR(ifname),
 	[USBG_F_NET_QMULT] = NET_DEC_ATTR(qmult),
-	[USBG_F_NET_CLASS] = NET_DEC_ATTR(class_),
+	[USBG_F_NET_CLASS] = NET_DEC_ATTR_(class, class_),
 	[USBG_F_NET_SUBCLASS] = NET_DEC_ATTR(subclass),
 	[USBG_F_NET_PROTOCOL] = NET_DEC_ATTR(protocol)
 };